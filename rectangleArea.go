a30226e824b4ea59fdf93213fc327b0240003025
JMboya <mboyajeff88@gmail.com>
2024-01-20 14:17:39 +0300

    Added area of a rectangle calculator using GO constants

---
+++ rectangleArea.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Constants for the length and width of the rectangle
+	const LENGTH = 10
+	const WIDTH = 5
+
+	// Calculate the area of the rectangle
+	area := LENGTH * WIDTH
+
+	// Display the result
+	fmt.Printf("Rectangle Area: %d\n", area)
+}
