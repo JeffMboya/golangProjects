bc21ecc3b3537133a42d28f8d02807df82c6417e
JMboya <mboyajeff88@gmail.com>
2024-01-20 13:56:10 +0300

    Added dtring formatter

---
+++ cicrleArea.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Declare a constant for pi
+const pi float64 = 3.142
+
+func main() {
+	//Ask the user for radius of the circle
+	fmt.Print("Enter radius of the circle")
+	var radius float64
+	fmt.Scan(&radius)
+
+	// Calculate area of the circle using the constant pi
+	area := pi * math.Pow(radius, 2)
+
+	//Display the results
+	fmt.Printf("The area of a circle of radius %.2f is %.2f \n", radius, area)
+}
